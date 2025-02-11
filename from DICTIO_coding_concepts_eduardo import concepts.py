43774777c363db0911f8b0f3c55794cce7ebbe78
eduardocerri <eduardocerrigaviria@gmail.com>
2025-02-11 00:09:22 -0400

    Create DICTIO_coding_concepts_eduardo.py

---
+++ from DICTIO_coding_concepts_eduardo import concepts.py
@@ -0,0 +1,137 @@
+concepts = {
+    "algorithm": "A step-by-step procedure for solving a problem:",    
+    "active record": '''The interface between a database and logic code. It takes data from 
+    databases, classifies it, and creates tables; common in Ruby on Rails:''',
+    "api": '''Acronym for Application Programming Interface: The software intermediary for 
+    two or more computer programs communicating with each other:''',
+    "argument": "A number, text string, or other value required for a function to run its code:",
+    "arithmetic operators": "Operators that are used with numbers to perform basic math:",
+    "array": "A data structure consisting of a collection of elements (values or variables):",
+    "ascii": "Acronym for American Standard Code for Information Interchange:", 
+    "back end": '''The server side of web development that focuses on parts of the website 
+    users do not see such as website architecture, scripting, and databases:''',
+    "binary": "A system of two possible states, zero and one:",
+    "bit": '''A single 0 or 1. It’s the smallest unit of information in computing and 
+    digital communications:''',
+    "boolean": "A data type that can only have one of two possible values: true or false:",
+    "bug": "Broken code that causes a program to malfunction:",
+    "byte": '''Unit of digital information that is typically eight bits long.
+    For example: 0000 0001:''',
+    "class": '''A template used to create and define the structure and behavior of a set of
+    objects. It encapsulates attributes and methods that characterize a type of object:''',
+    "code": "The written content of a computer program:", 
+    "command": "An order the computer must carry out:",
+    "compilation": "The procedure that translates code into a format the computer can use:",
+    "compiler": '''A program that changes text-based code into the code a computer understands. 
+    The result is an application, often a .exe file:''',
+    "computational thinking": '''A problem-solving approach that involves breaking down complex
+    problems into smaller more manageable parts, identifying patterns and abstractions, and 
+    developing algorithmic solutions:''',
+    "computer science": '''The systematic study of algorithmic processes that describe and 
+    transform information: their theory, analysis, design, efficiency, implementation
+    and application:''',
+    "conditional": "Statement that only run under certain conditions:",
+    "constant": '''A number, text string, or symbol that never changes value while a program
+    is running, the oposite of a variable:''',    
+    "cybersecurity": '''A field of computing that deals with the safety of anything stored
+    on a computer:''',
+    "css": '''Acronym for Cascading Style Sheet, a stylesheet language that helps format
+    HTML webpages:''',
+    "data": "Information. Often, quantities, characters, or symbols:",
+    "debugging": "The process of looking for and repairing coding errors in a program:",
+    "declaration": '''A single word or symbol used to describe a function or variable. 
+    It defines the type of variable or function so the compiler or interpreter knows what 
+    to do with it:''',
+    "dns": "The service that translates URLs to IP addresses:",
+    "database": "A collection or set of data used for access, management, and updates:",
+    "data structures": "The format used to organize, manage, and store data:",
+    "dynamic language": '''A programming language that verifies and enforces constraints at
+    runtime instead of beforehand, such as Ruby and Python:''',
+    "event": "An action that causes something to happen:",
+    "exception": "An unplanned event that disrupts the normal flow of the program:",
+    "for loop": "A loop with a predetermined beginning, end, and increment (step interval):", 
+    "function": "A piece of code that you can easily call over and over again:", 
+    "framework": '''A collection of tools, libraries, designs, and organisational principles
+    that assist product development:''',
+    "front end": "The user side of web development that focuses on appearance and functionality:",
+    "gui": "Stands for graphical user interface:",
+    "hardcode": "Permanent code. Code that a programmer can’t change easily or at all:", 
+    "html": "Stands for Hypertext Markup Language. Uses tags to define elements within a document:",
+    "http": '''Stands for Hypertext Transfer Protocol for the web. Outlines the data transmission 
+    and reception between browsers and servers:''',
+    "inheritance": "The practice of basing a new piece of code on existing code:",
+    "integrated development environment": '''Software used to build applications that combines
+    code editor, compiler/interpreter, debugger among other tools:''',
+    "identifier": '''A name given to a specific program element, such as Variables, Constants,
+    Functions, Classes:''',
+    "if statement": "The common programming structure that implements 'conditional statements':",
+    "interpreter": "A program that executes code directly, line by line:",
+    "input": "A way to give information to a computer:", 
+    "infinite loop": '''Coding that lacks a functional exit or ending condition and 
+    repeats indefinitely:''',
+    "iteration": "A repetitive action or command typically created with programming loops:", 
+    "loop": "The action of doing something over and over again:",
+    "keywords": '''Predefined words in a programming language that has a predefined meaning and
+    reserved use:''',
+    "linux": '''A powerful open-source operating system known for its flexibility, stability
+    and security:''',
+    "library": '''A collection of resources for computer programs and software development including
+    prewritten code:''',
+    "mysql": '''A software system designed to store, manage, and retrieve data efficiently
+    in databases:''',
+    "machine language": '''The lowest-level programming language and the most basic level of 
+    instructions that a computer can directly understand. It consists of binary instructions:''',
+    "namespace": '''A mechanism for organizing code elements (like classes, functions, and variables)
+    within a program to avoid naming conflicts. It creates a unique scope for these elements, 
+    preventing clashes with elements of the same name in other parts of the program:''',
+    "null": '''A keyword or character indicating something has zero value or that the value
+    is unknown or the absence of data:''',
+    "object": '''An instance of a class. It represents a specific entity with its own unique
+    set of data values for the attributes defined by the class:''',
+    "output": '''The content or data that comes out of a computer after being processed or generated
+    by it:''',
+    "package": "Also called namespaces, used to identify related classes and interfaces:",
+    "packets": "A formatted group of data transferred over a computer network:",
+    "parameter": '''The input of a function. It gets replaced by an argument when the function
+    is called:''',
+    "pixel": '''Short for 'picture element', the fundamental unit of a digital image, a tiny dot
+    that forms a larger image:''',
+    "program": "Written code that runs on a computer:", 
+    "programming": "The art of creating a program:", 
+    "persitence": "When a piece of data, information, or web page remains accessible:",
+    "pointer": "It points to a specific value stored at a specific address in a computer's memory:",
+    "repository": '''A central location where developers store and manage the source code of a
+    software project:''',
+    "script": "A small program that do limited steps. It can be part of a bigger program:",
+    "search engine": '''A program that searches for items in a database that correspond to keywords
+    specified by the user:''',
+    "source code": '''All the human-readable text files written in a programming language that are
+    necessary to build and run a software application:''',
+    "server": '''The computer program or device that send data to a network, another computer
+    and to users:''',
+    "stack": '''A structure of elements that operates on the LIFO (last-in, first-out)
+    programming principle:''',
+    "static language": '''A programming language that verifies and enforces type constraints at 
+    compile time such as C++, C, and Java:''',
+    "sql": '''The most popular programming language for adding and retrieving information from a
+    relational database:''',
+    "syntax": """The structure of a language. The rules that state in what order words must appear. 
+    Each programming language has its own:""",
+    "terminal": '''A text-based interface for interacting with a computer
+    allowing you to execute commands, manage files, and perform various tasks:''',
+    "token": '''The smallest meaningful unit of a program's source code, like Keywords, Identifiers,
+    Operators, Literals, and Special Characters:''',
+    "url": '''Stands for universal resource locator. A reference to the address used to locate
+    a resource on the internet:''',
+    "variable": "A placeholder for a piece of information that can change:",
+    "website": "A collection of interlinked web pages on the World Wide Web:", 
+    "while loop": "A loop that continues to repeat while a condition is true:", 
+    "whiteboarding": '''Writing code on a whiteboard, often part of technical interviews and
+    group projects:''',
+    "xml": "Acronym for extensible markup language:",
+  
+
+    }
+
+
+
