b50e219ef9e7cdd31d1dc4c6f732d497fff5d257
independentway <eduardocerrigaviria@gmail.com>
2025-02-19 22:30:50 -0400

    Update beta.py

--- beta.py
+++ beta.py
@@ -1,32 +1,29 @@
 import random
 import tkinter as tk
 from diccionariodepruebas import concepts
+import copy 
 
-# Starting global variables
+copy_dictionary = copy.deepcopy(concepts) # copying the dictionary to avoid modifying the original one
+
+random_key = random.choice(list(copy_dictionary.keys())) # turning the dictionary into a list and choosing a random key
+root = tk.Tk()
+root.title("CQ: Concept Quiz")
+root.geometry("850x230")  
+root.resizable(False, False) 
 
 total_concepts = len(concepts)
 correct_answers = 0
 wrong_answers = 0
-root = tk.Tk()
-root.title("CQ: Concept Quiz")
-random_key = random.choice(list(concepts.keys()))
+
 my_font = ("Terminal", 10)
-
-
-# Stabilishing fixed size of window
-
-root.geometry("850x230")  
-root.resizable(False, False) # avoiding automate resizing 
-
 
 # Defining the function check_answer 
 
 def check_answer(event=None):
-    global random_key, correct_answers, wrong_answers, button_result, label_result, label_end_of_quiz
+    global random_key, correct_answers, wrong_answers, button_result, label_result, label_end_of_quiz, random_key, copy_dictionary
 
     # Obtaining user answer
     user_answer = entry_answer.get().lower()    
-
 
     # Verifying answer
     if user_answer == random_key:        
@@ -38,15 +35,17 @@ def check_answer(event=None):
         label_answer.config(text=f'''Wrong Answer!
 The Correct Answer is: {random_key}''')
         wrong_answers += 1  # Increment wrong answers
+
+    del copy_dictionary[random_key] # deleting already asked item to empty the dictionary and not repeat it
          
     # Generating new question
-    random_key = random.choice(list(concepts.keys()))
-    label_question.config(text=concepts[random_key])
+    random_key = random.choice(list(copy_dictionary.keys()))
+    label_question.config(text=copy_dictionary[random_key])
     label_previous_answer.config(text=f"Previous Answer: {entry_answer.get()}")
     entry_answer.delete(0, tk.END)
     
     # Binding result button to result page
-    if correct_answers + wrong_answers == total_concepts + 1:
+    if correct_answers + wrong_answers == total_concepts -1:
 
         label_question.pack_forget()
         entry_answer.pack_forget()
@@ -60,12 +59,13 @@ The Correct Answer is: {random_key}''')
         label_end_of_quiz.pack(padx=20, pady=40)
         button_result = tk.Button(root, text="Show Results", command=show_result_page, font=my_font)
         button_result.pack(padx=0, pady=5)
+        copy_dictionary = copy.deepcopy(concepts) # resetting the dictionary to its original state
 
     
 # Creating function Result Page and its elements elements + hiding main quiz ones
 
 def show_result_page():
-    global correct_answers, wrong_answers, label_question, label_previous_answer, entry_answer, button_send, label_answer, label_result, button_result, button_restart, main_quiz
+    global correct_answers, wrong_answers, label_question, label_previous_answer, entry_answer, button_send, label_answer, label_result, button_result, button_restart, copy_dictionary, main_quiz
 
     # Hide main quiz elements
     label_question.pack_forget()
@@ -88,7 +88,7 @@ def show_result_page():
 # Creating main quiz
 
 def main_quiz():
-    global label_question, label_previous_answer, entry_answer, button_send, label_answer, button_restart, label_result         
+    global label_question, label_previous_answer, entry_answer, button_send, label_answer, button_restart, label_result, copy_dictionary         
        
     label_welcome.pack_forget()
     button_start.pack_forget()  
@@ -105,7 +105,7 @@ def main_quiz():
     entry_answer.bind("<Return>", check_answer) 
 
 def restart_quiz():
-    global correct_answers, wrong_answers, random_key
+    global correct_answers, wrong_answers, random_key, copy_dictionary
 
     button_restart.pack_forget()
     label_result.pack_forget()
@@ -113,7 +113,7 @@ def restart_quiz():
     # Reset variables
     correct_answers = 0
     wrong_answers = 0
-    random_key = random.choice(list(concepts.keys()))
+    random_key = random.choice(list(copy_dictionary.keys()))
     label_answer.config(text="")
     label_previous_answer.config(text="")
 
@@ -125,7 +125,7 @@ def restart_quiz():
     label_answer.pack()
 
     # Update question and previous answer labels
-    label_question.config(text=concepts[random_key])
+    label_question.config(text=copy_dictionary[random_key])
 
     entry_answer.delete(0, tk.END)  # Clear entry
 
@@ -133,7 +133,7 @@ def restart_quiz():
 # Creating main quiz elements
 
 entry_answer = tk.Entry(root, font=my_font)
-label_question = tk.Label(root, text=concepts[random_key], font=my_font)
+label_question = tk.Label(root, text=copy_dictionary[random_key], font=my_font)
 label_previous_answer = tk.Label(root, text=f"Previous Answer: {entry_answer.get()}", font=my_font)
 label_answer = tk.Label(root, font=my_font)
 button_send = tk.Button(root, text="Send", command=check_answer, font=my_font) 
@@ -157,10 +157,5 @@ button_start.pack(padx=0, pady=1)
 # Starting main loop
 
 root.mainloop()
-
-
-
-
-
 
 
